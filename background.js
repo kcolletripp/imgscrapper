a764916105a6cae936f30bf84a6576e1ce9f3b61
kcolletripp <kcolletripp@gmail.com>
2015-12-24 23:51:51 -0500

    now downloads to downloads/img folder

--- background.js
+++ background.js
@@ -14,7 +14,14 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "open_new_tab" ) {
       //chrome.tabs.create({"url": request.url});
-      chrome.downloads.download({ url: request.url })
+
+      //this removes the anchor at the end, if there is one
+      var path = request.url.substring(0, (request.url.indexOf("#") == -1) ? request.url.length : request.url.indexOf("#"));
+      //this removes the query after the file name, if there is one
+      path = path.substring(0, (path.indexOf("?") == -1) ? path.length : path.indexOf("?"));
+      path = "img/" + path.substring(path.lastIndexOf('/')+1);
+      console.log(path);
+      chrome.downloads.download({ url: request.url, filename: path })
     }
   }
 );
